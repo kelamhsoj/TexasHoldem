2718a1ff6ad08344c86b29364d9e28385f307ff8
Jatin Bharwani <jatinbharwani@gmai.com>
2015-12-04 15:30:55 -0500

    Added more to main.ml

--- main.ml
+++ main.ml
@@ -34,9 +34,12 @@ let init_game () =  let human = create_human () in
   let ai4 = create_ai () in
   let playingdeck = newdeck () in
   let newgamestate = {pot = 0; deck = playingdeck; table=[];
-  players= [human; ai1; ai2; ai3; ai4]; bet = 0; mode = Welcome } in
+  players= [human; ai1; ai2; ai3; ai4]; bet = 1; mode = Welcome } in
   newgamestate
 
+let add_cardto_table gstate =
+  let old = gstate.table in
+  gstate.table <- old @ (pop gstate.deck 1)
 (*Prints promp and returns the action option of what the human wants to do*)
 let cycle_user_input (prompt: string): action option =
   print_string prompt;
@@ -80,7 +83,18 @@ let printhands gstate =
 (*Welcomes User, ask if they would like to play*)
 let rec welcome_user gstate =
   print_endline "Welcome to Texas Holdem!";
-  gstate.mode <- Init;
+  print_endline "Ready to play against 4 Players?";
+  let rec input_helper () =
+    let result = read_line() in
+    let result' = String.lowercase result in
+    match result' with
+      | "yes" -> print_endline "Ok, starting game! You are Player 0";
+                gstate.mode <- Init
+      | "no" -> print_endline "Ok, quitting"
+               (*quit*)
+      | _ -> print_endline "It was a yes or no question.";
+             input_helper () in
+  input_helper ();
   engine gstate
 
 (* *Deal players two cards
@@ -92,10 +106,23 @@ and deal (gstate: gamestate) =
   print_endline "Dealing Cards";
   for x = 0 to ((List.length gstate.players)-1) do
     let p = (List.nth gstate.players x) in
-    p.cards <- (Deck.pop gstate.deck 2)
+    p.cards <- (Deck.pop gstate.deck 2);
+    if p.human then (print_endline "Your hand is:"; printcardlist p.cards;
+                print_newline ()) else ()
   done;
   gstate.mode <- Preflop;
   engine gstate
+
+and run_cycle gstate =
+  while (everyone_same_bet gstate.bet gstate.players) <> true do
+    cycle gstate
+  done;
+  if gstate.mode = Preflop then gstate.mode <- Flop
+  else if gstate.mode = Flop then gstate.mode <- Turn
+  else if gstate.mode = Turn then gstate.mode <- River
+  else if gstate.mode = River then gstate.mode <- End
+  else failwith "done"
+
 
 (*Go through player list
   If player is a human, prompt user input (call, fold, or raise)
@@ -107,7 +134,8 @@ and cycle gstate =
   for x = 0 to ((List.length gstate.players)-1) do
     let p = (List.nth gstate.players x) in
     match p.state with
-    | Folded | Allin -> ()
+    | Folded -> print_endline ("Player " ^ (string_of_int x) ^ " is still out because she folded")
+    | Allin -> print_endline ("Player " ^ (string_of_int x) ^ " is still all in")
     | Playing -> if p.human = true then
                   (match getuserdecision gstate p with
                   | None ->  print_endline "Command not recognized. Please try again";
@@ -144,11 +172,7 @@ and cycle gstate =
                      | Raise y -> gstate.bet <- gstate.bet + y;
                                   gstate.pot <- gstate.pot + (gstate.bet - bet))
   done;
-  if gstate.mode = Preflop then gstate.mode <- Flop
-  else if gstate.mode = Flop then gstate.mode <- Turn
-  else if gstate.mode = Turn then gstate.mode <- River
-  else if gstate.mode = River then gstate.mode <- End
-  else failwith "error"
+
 
 (* Hands are shown, pot is emptied, score of winner is updated
   If any players are eliminated, they are removed from the table
@@ -157,20 +181,39 @@ and cycle gstate =
   and stage goes back to init
 *)
 and finish_round gstate =
-  failwith "TODO"
-  (*printhands gstate;
-  gstate.mode <- Init;
-  engine gstate*)
+  for x = 0 to ((List.length gstate.players)-1) do
+    let p = (List.nth gstate.players x) in
+    match p.state with
+      | Folded -> ()
+      | Allin  -> failwith "TODO"
+      | Playing -> failwith "TODO"
+
+  done;
+  print_endline "Would you like to play another round?";
+  let rec input_helper () =
+    let result = read_line() in
+    let result' = String.lowercase result in
+    match result' with
+      | "yes" -> print_endline "Ok, starting new round!";
+                gstate.mode <- Init
+      | "no" -> print_endline "Ok, quitting"
+               (*quit*)
+      | _ -> print_endline "It was a yes or no question.";
+             input_helper () in
+
+  input_helper ();
+  engine gstate
+
 
 (*Main function that runs based on state of the game*)
 and engine gstate =
   match gstate.mode with
     | Welcome -> welcome_user gstate
     | Init -> deal gstate
-    | Preflop -> cycle gstate
-    | Flop -> (*Add a card to the table*) cycle gstate
-    | Turn -> (*Add a card to the table*) cycle gstate
-    | River -> (*Add a card to the table*) cycle  gstate
+    | Preflop -> run_cycle gstate
+    | Flop -> add_cardto_table gstate; run_cycle gstate
+    | Turn -> add_cardto_table gstate; run_cycle gstate
+    | River -> add_cardto_table gstate; run_cycle  gstate
     | End -> finish_round gstate
 
 let _ =
